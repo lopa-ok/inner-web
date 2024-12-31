c8e91c151c31a57e90e1371e31f8a054c1ca4743
philopater <lopa@programmer.net>
2024-12-31 22:07:56 +0200

    Update VerticalNavbar.tsx

--- src/components/showcase/VerticalNavbar.tsx
+++ src/components/showcase/VerticalNavbar.tsx
@@ -69,7 +69,7 @@ const VerticalNavbar: React.FC<VerticalNavbarProps> = (props) => {
                             />
                             <Link
                                 containerStyle={styles.insetLink}
-                                to="projects/art"
+                                to="projects/games"
                                 text="GAMES"
                             />
                         </div>
