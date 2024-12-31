901b064c6c9a377343909f213cf375e7af8953a2
philopater <lopa@programmer.net>
2024-12-31 21:22:01 +0200

    test

--- src/components/showcase/Projects.tsx
+++ src/components/showcase/Projects.tsx
@@ -75,7 +75,7 @@ const Projects: React.FC<ProjectsProps> = (props) => {
                 <ProjectBox
                     title="Games"
                     subtitle=""
-                    route="Games"
+                    route="games"
                 />
             </div>
         </div>
--- src/components/showcase/VerticalNavbar.tsx
+++ src/components/showcase/VerticalNavbar.tsx
@@ -69,7 +69,7 @@ const VerticalNavbar: React.FC<VerticalNavbarProps> = (props) => {
                             />
                             <Link
                                 containerStyle={styles.insetLink}
-                                to="projects/Games"
+                                to="projects/games"
                                 text="GAMES"
                             />
                         </div>
