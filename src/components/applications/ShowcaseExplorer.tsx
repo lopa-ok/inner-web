bdc7fab168baa6c4910a4bb4f9b984450c1e5034
philopater <lopa@programmer.net>
2024-12-31 21:47:47 +0200

    Update ShowcaseExplorer.tsx

--- src/components/applications/ShowcaseExplorer.tsx
+++ src/components/applications/ShowcaseExplorer.tsx
@@ -9,7 +9,7 @@ import Contact from '../showcase/Contact';
 import Hack from '../showcase/Hack';
 import SoftwareProjects from '../showcase/projects/software';
 import MusicProjects from '../showcase/projects/robotics';
-import ArtProjects from '../showcase/projects/Art';
+import ArtProjects from '../showcase/projects/art';
 import VerticalNavbar from '../showcase/VerticalNavbar';
 import useInitialWindowSize from '../../hooks/useInitialWindowSize';
 
