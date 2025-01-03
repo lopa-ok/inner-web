fc94bdebbaa4fdbe028741756efd759b229eaccc
philopater <lopa@programmer.net>
2025-01-04 01:15:05 +0200

    test 2

--- src/components/applications/FileExplorer.tsx
+++ src/components/applications/FileExplorer.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import Window from '../os/Window';
 import folderIcon from '../../assets/icons/scrabbleIcon.png';
 
-export interface FileExplorerProps extends WindowAppProps {}
+export interface FileExplorerProps extends WindowAppProps {
+    minimizeWindow: () => void;
+}
 
 const FileExplorer: React.FC<FileExplorerProps> = (props) => {
     return (
@@ -11,36 +13,36 @@ const FileExplorer: React.FC<FileExplorerProps> = (props) => {
             left={50}
             width={800}
             height={600}
-            windowBarIcon="windowGameIcon"
+            windowBarIcon="windowExplorerIcon"
             windowTitle="File Explorer"
             closeWindow={props.onClose}
             onInteract={props.onInteract}
-            minimizeWindow={props.onMinimize}
+            minimizeWindow={props.minimizeWindow}
         >
-            <div className="file-explorer">
-                <div className="sidebar">
-                    <div className="sidebar-item">Desktop</div>
-                    <div className="sidebar-item">Documents</div>
-                    <div className="sidebar-item">Downloads</div>
-                    <div className="sidebar-item">Pictures</div>
-                    <div className="sidebar-item">Music</div>
-                    <div className="sidebar-item">Videos</div>
+            <div className="file-explorer" style={styles.fileExplorer}>
+                <div className="sidebar" style={styles.sidebar}>
+                    <div className="sidebar-item" style={styles.sidebarItem}>Desktop</div>
+                    <div className="sidebar-item" style={styles.sidebarItem}>Documents</div>
+                    <div className="sidebar-item" style={styles.sidebarItem}>Downloads</div>
+                    <div className="sidebar-item" style={styles.sidebarItem}>Pictures</div>
+                    <div className="sidebar-item" style={styles.sidebarItem}>Music</div>
+                    <div className="sidebar-item" style={styles.sidebarItem}>Videos</div>
                 </div>
-                <div className="content">
-                    <div className="content-header">
-                        <div className="content-header-item">Name</div>
-                        <div className="content-header-item">Date Modified</div>
-                        <div className="content-header-item">Type</div>
-                        <div className="content-header-item">Size</div>
+                <div className="content" style={styles.content}>
+                    <div className="content-header" style={styles.contentHeader}>
+                        <div className="content-header-item" style={styles.contentHeaderItem}>Name</div>
+                        <div className="content-header-item" style={styles.contentHeaderItem}>Date Modified</div>
+                        <div className="content-header-item" style={styles.contentHeaderItem}>Type</div>
+                        <div className="content-header-item" style={styles.contentHeaderItem}>Size</div>
                     </div>
-                    <div className="content-body">
-                        <div className="content-item">
-                            <img src={folderIcon} alt="Folder" className="content-item-icon" />
-                            <span className="content-item-name">My Documents</span>
+                    <div className="content-body" style={styles.contentBody}>
+                        <div className="content-item" style={styles.contentItem}>
+                            <img src={folderIcon} alt="Folder" className="content-item-icon" style={styles.contentItemIcon} />
+                            <span className="content-item-name" style={styles.contentItemName}>My Documents</span>
                         </div>
-                        <div className="content-item">
-                            <img src={folderIcon} alt="Folder" className="content-item-icon" />
-                            <span className="content-item-name">My Pictures</span>
+                        <div className="content-item" style={styles.contentItem}>
+                            <img src={folderIcon} alt="Folder" className="content-item-icon" style={styles.contentItemIcon} />
+                            <span className="content-item-name" style={styles.contentItemName}>My Pictures</span>
                         </div>
                         {/* Add more items as needed */}
                     </div>
